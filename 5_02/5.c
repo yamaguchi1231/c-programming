8fd9b7a76188b46cf8b3a0816bd1247ba56e2574
yamaguchi1231 <ggf-600-vts@e-net.nara.jp>
2025-05-02 06:14:00 +0000

    5月2日

---
+++ 5_02/5.c
@@ -0,0 +1,15 @@
+
+#include <stdio.h>
+
+int main(void){
+    int score;
+    printf("点数を入力してください\n");
+    scanf("%d",&score);
+    if(score>=80){
+        printf("合格です\n");
+    }else{
+        printf("不合格です\n");
+    }
+
+    return 0;
+}
---
+++ 5_02/8.c
@@ -0,0 +1,21 @@
+
+#include <stdio.h>
+
+int main(void){
+  //  for(int i=1;i<=10;i++){
+  //      for(int j=1;j<=10;j++){
+  //          printf("%4d",1*j);
+  //      }
+  //      printf("\n");
+  //  }
+
+    for(int i=1;i<=10;i++){
+        for(int j=1;j<=10;j++){
+            printf("%d*%d=%d\n",i,j,i*j);
+        }
+    }
+        
+
+
+    return 0;
+}
